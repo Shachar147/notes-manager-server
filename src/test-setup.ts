f9f67ebc6daf7d109e09021d6e6ef07ad3f6ad03
Shachar <shachar147@gmail.com>
2025-06-25 14:46:18 +0300

    wip

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,3 +1,20 @@
 // src/test-setup.ts
 // Suppress console.error messages during tests
-jest.spyOn(console, 'error').mockImplementation(() => {}); 
+jest.spyOn(console, 'error').mockImplementation(() => {});
+
+// Mock all console methods to suppress log output in tests
+const originalConsole = { ...console };
+beforeAll(() => {
+  console.log = jest.fn();
+  console.error = jest.fn();
+  console.warn = jest.fn();
+  console.info = jest.fn();
+  console.debug = jest.fn();
+});
+afterAll(() => {
+  console.log = originalConsole.log;
+  console.error = originalConsole.error;
+  console.warn = originalConsole.warn;
+  console.info = originalConsole.info;
+  console.debug = originalConsole.debug;
+}); 
