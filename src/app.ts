0ae889430b4db641a3b0b36bec58bdbe3d53fc3a
Shachar <shachar147@gmail.com>
2025-06-25 14:42:09 +0300

    fix saving a note with a lot of content

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,8 @@ import { NoteChatbotUsageService } from './features/notes/notes-chatbot-usage.se
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 app.use(requestIdMiddleware);
 app.use(elasticLoggerMiddleware); // Request logging middleware
